c302d13465e08a0dc2f4946898d90b8b7a36dd35
Nikolay Semashko <n_semashko@lesta.group>
2024-01-16 15:37:34 +0300

    Dev: del and update user

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import date
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -15,16 +15,44 @@ class User(db.Model):
     first_name = db.Column(db.String(45), nullable=True)
     last_name = db.Column(db.String(45), nullable=True)
     gender = db.Column(db.String(1), nullable=True)
-   # birthday = db.Column(db.DateTime, nullable=False)
+    birthday = db.Column(db.Date, nullable=False)
+
 
     def __repr__(self):
         return '<User %r>' % self.user_id
 
 
+class Kon(db.Model):
+    kon_id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
+    round = db.Column(db.Integer, nullable=True)
+    comment = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Kon %r>' % self.kon_id
+
+
+class Result(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    kon_id = db.Column(db.Integer, primary_key=True)
+    result = db.Column(db.Integer,  nullable=True)
+    is_winner = db.Column(db.Boolean, nullable=True)
+    price = db.Column(db.Integer, nullable=True)
+
+    fk_user = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('kons', lazy=True))
+
+    fk_kon = db.Column(db.Integer, db.ForeignKey('kon.kon_id'), nullable=False)
+    kon = db.relationship('Kon', backref=db.backref('results', lazy=True))
+
+    def __repr__(self):
+        return '<Result %r>' % self.user_id
+
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template("index.html")
+
 
 @app.route('/users')
 def users():
@@ -32,11 +60,56 @@ def users():
     return render_template("users.html", user=user)
 
 
+@app.route('/kons')
+def kons():
+    kon = Kon.query.order_by(Kon.kon_id).all()
+    return render_template("kons.html", kon=kon)
+
+
+@app.route('/results')
+def results():
+    result = Result.query.order_by(Result.user_id).all()
+    return render_template("results.html", result=result)
+
+# детали по пользователю
 @app.route('/users/<int:user_id>')
 def users_detail(user_id):
     user_det = User.query.get(user_id)
     return render_template("user_detail.html", user_det=user_det)
 
+
+# удаление пользователя из таблицы
+@app.route('/users/<int:user_id>/delete')
+def users_delete(user_id):
+    user_det = User.query.get_or_404(user_id)
+
+    try:
+        db.session.delete(user_det)
+        db.session.commit()
+        return redirect('/users')
+    except:
+        return "Delete user was mistake"
+
+
+# редактирование пользователя из таблицы
+@app.route('/users/<int:user_id>/update', methods=['POST', 'GET'])
+def user_update(user_id):
+    user_det = User.query.get(user_id)
+    if request.method == "POST":
+        user_det.nickname = request.form['nickname']
+        user_det.first_name = request.form['first_name']
+        user_det.last_name = request.form['last_name']
+        user_det.gender = request.form['gender']
+        user_det.birthday = datetime.strptime(request.form['birthday'], "%Y-%m-%d")
+
+
+        try:
+            db.session.commit()
+            return redirect('/users')
+        except:
+            return "Add User was ERROR"
+    else:
+        return render_template("user_update.html", user_det=user_det)
 
 @app.route('/about')
 def about():
@@ -51,9 +124,8 @@ def Add_user():
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         gender = request.form['gender']
-        #birthday = request.form['birthday']
-        #birthday = date(2023, 3, 1)
-        user = User(nickname=nickname, first_name=first_name, last_name=last_name, gender=gender) #birthday=birthday)]
+        birthday = datetime.strptime(request.form['birthday'], "%Y-%m-%d")
+        user = User(nickname=nickname, first_name=first_name, last_name=last_name, gender=gender, birthday=birthday)
 
 
         try:
