dd3deeb735407f9f29aaebc679c39aadc8021efa
Nikolay Semashko <n_semashko@lesta.group>
2024-01-18 11:07:47 +0300

    DEv: add kon

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///game.db'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Qwert123@localhost/game1.db'
 db = SQLAlchemy(app)
 
-
+# таблица Юзеры
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(45), nullable=True)
@@ -21,17 +21,17 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.user_id
 
-
+# таблица игр которые будут добавляться
 class Kon(db.Model):
     kon_id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, nullable=False)
+    date = db.Column(db.Date, nullable=False)
     round = db.Column(db.Integer, nullable=True)
     comment = db.Column(db.String(250), nullable=False)
 
     def __repr__(self):
         return '<Kon %r>' % self.kon_id
 
-
+# таблица результатов по играм
 class Result(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     kon_id = db.Column(db.Integer, primary_key=True)
@@ -53,30 +53,39 @@ class Result(db.Model):
 def index():
     return render_template("index.html")
 
-
+# страница юзера
 @app.route('/users')
 def users():
     user = User.query.order_by(User.nickname).all()
     return render_template("users.html", user=user)
 
+# Добавление пользователей на страницу пользователей через кнопку добавления
+@app.route('/Add_user', methods=['POST', 'GET'])
+def Add_user():
+    if request.method == "POST":
+        #user_id = request.form['user_id']
+        nickname = request.form['nickname']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        gender = request.form['gender']
+        birthday = datetime.strptime(request.form['birthday'], "%Y-%m-%d")
+        user = User(nickname=nickname, first_name=first_name, last_name=last_name, gender=gender, birthday=birthday)
 
-@app.route('/kons')
-def kons():
-    kon = Kon.query.order_by(Kon.kon_id).all()
-    return render_template("kons.html", kon=kon)
 
-
-@app.route('/results')
-def results():
-    result = Result.query.order_by(Result.user_id).all()
-    return render_template("results.html", result=result)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return redirect('/users')
+        except:
+            return "Add User was ERROR"
+    else:
+        return render_template("Add_user.html")
 
 # детали по пользователю
 @app.route('/users/<int:user_id>')
 def users_detail(user_id):
     user_det = User.query.get(user_id)
     return render_template("user_detail.html", user_det=user_det)
-
 
 # удаление пользователя из таблицы
 @app.route('/users/<int:user_id>/delete')
@@ -111,31 +120,84 @@ def user_update(user_id):
     else:
         return render_template("user_update.html", user_det=user_det)
 
+@app.route('/kons')
+def kons():
+    kon = Kon.query.order_by(Kon.kon_id).all()
+    return render_template("kons.html", kon=kon)
+
+# Добавление игры в базу kon, через кпопку добавить
+@app.route('/add_kon', methods=['POST', 'GET'])
+def add_kon():
+    if request.method == "POST":
+        #kon_id = request.form['kon_id']
+        date = datetime.strptime(request.form['date'], "%Y-%m-%d")
+        round = request.form['round']
+        comment = request.form['comment']
+        kon = Kon(date=date, round=round, comment=comment)
+
+
+        try:
+            db.session.add(kon)
+            db.session.commit()
+            return redirect('/kons')
+        except:
+            return "Add Kon was ERROR"
+    else:
+        return render_template("add_kon.html")
+
+# детали по игре
+@app.route('/kons/<int:kon_id>')
+def kon_detail(kon_id):
+    kon_det = Kon.query.get(kon_id)
+    return render_template("kon_detail.html", kon_det=kon_det)
+
+
+# удаление игры из таблицы
+@app.route('/kons/<int:kon_id>/delete')
+def kon_delete(kon_id):
+    kon_det = Kon.query.get_or_404(kon_id)
+
+    try:
+        db.session.delete(kon_det)
+        db.session.commit()
+        return redirect('/kons')
+    except:
+        return "Delete kon was mistake"
+
+
+# редактирование игры из таблицы
+@app.route('/kons/<int:kon_id>/update', methods=['POST', 'GET'])
+def kon_update(kon_id):
+    kon_det = Kon.query.get(kon_id)
+    if request.method == "POST":
+        #kon_det.kon_id = request.form['kon_id']
+        kon_det.date = datetime.strptime(request.form['date'], "%Y-%m-%d")
+        kon_det.round = request.form['round']
+        kon_det.comment = request.form['comment']
+
+        try:
+            db.session.commit()
+            return redirect('/kons')
+        except:
+            return "Add Kon was ERROR"
+    else:
+        return render_template("kon_update.html", kon_det=kon_det)
+
+@app.route('/results')
+def results():
+    result = Result.query.order_by(Result.user_id).all()
+    return render_template("results.html", result=result)
+
+
+
 @app.route('/about')
 def about():
     return render_template("about.html")
 
 
-@app.route('/Add_user', methods=['POST', 'GET'])
-def Add_user():
-    if request.method == "POST":
-        #user_id = request.form['user_id']
-        nickname = request.form['nickname']
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        gender = request.form['gender']
-        birthday = datetime.strptime(request.form['birthday'], "%Y-%m-%d")
-        user = User(nickname=nickname, first_name=first_name, last_name=last_name, gender=gender, birthday=birthday)
 
 
-        try:
-            db.session.add(user)
-            db.session.commit()
-            return redirect('/users')
-        except:
-            return "Add User was ERROR"
-    else:
-        return render_template("Add_user.html")
+
 
 
 if __name__ == "__main__":
